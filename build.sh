41bd28b352dd24a53fa6bacc4fcdb8ed52e07695
Alexey Proshutinskiy <alexey.prosh@fluence.one>
2021-05-25 13:01:21 +0300

    add service id and name to generated .aqua

--- build.sh
+++ build.sh
@@ -9,4 +9,4 @@ rm -f artifacts/*
 mkdir -p artifacts
 cp target/wasm32-wasi/release/aqua-dht.wasm artifacts/
 curl -L https://github.com/fluencelabs/sqlite/releases/download/v0.14.0_w/sqlite3.wasm -o artifacts/sqlite3.wasm
-marine aqua artifacts/aqua-dht.wasm > aqua/aqua-dht.aqua
+marine aqua artifacts/aqua-dht.wasm -s AquaDHT -i aqua-dht > aqua/aqua-dht.aqua
