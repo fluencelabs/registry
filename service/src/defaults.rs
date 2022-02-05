845705df9868e8a24d83d0f559991e395b62810e
Aleksey Proshutisnkiy <justprosh@users.noreply.github.com>
2022-02-05 10:14:27 +0300

    scheduled script fix (#83)

--- service/src/defaults.rs
+++ service/src/defaults.rs
@@ -22,7 +22,7 @@ pub static DB_PATH: &str = "/tmp/dht.db";
 pub static DEFAULT_STALE_VALUE_AGE: u64 = 60 * 60;
 pub static DEFAULT_EXPIRED_VALUE_AGE: u64 = 24 * 60 * 60;
 pub static DEFAULT_EXPIRED_HOST_VALUE_AGE: u64 = 10 * DEFAULT_EXPIRED_VALUE_AGE;
-pub static VALUES_LIMIT: usize = 20;
+pub static VALUES_LIMIT: usize = 32;
 
 pub static TRUSTED_TIMESTAMP_SERVICE_ID: &str = "peer";
 pub static TRUSTED_TIMESTAMP_FUNCTION_NAME: &str = "timestamp_sec";
