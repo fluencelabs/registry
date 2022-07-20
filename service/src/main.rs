f2640cf372be869c568057665fe0ba49e590390d
Aleksey Proshutisnkiy <justprosh@users.noreply.github.com>
2022-07-20 15:23:43 +0400

    ci: move from circle to gh (#112)

--- service/src/main.rs
+++ service/src/main.rs
@@ -40,15 +40,15 @@ mod tetraplets_checkers;
 extern crate fstrings;
 
 /*
-    _initialize function that calls __wasm_call_ctors is required to mitigade memory leak
-    that is described in https://github.com/WebAssembly/wasi-libc/issues/298
+   _initialize function that calls __wasm_call_ctors is required to mitigade memory leak
+   that is described in https://github.com/WebAssembly/wasi-libc/issues/298
 
-    In short, without this code rust wraps every export function
-    with __wasm_call_ctors/__wasm_call_dtors calls. This causes memory leaks. When compiler sees
-    an explicit call to __wasm_call_ctors in _initialize function, it disables export wrapping.
+   In short, without this code rust wraps every export function
+   with __wasm_call_ctors/__wasm_call_dtors calls. This causes memory leaks. When compiler sees
+   an explicit call to __wasm_call_ctors in _initialize function, it disables export wrapping.
 
-    TODO: remove when updating to marine-rs-sdk with fix
- */
+   TODO: remove when updating to marine-rs-sdk with fix
+*/
 extern "C" {
     pub fn __wasm_call_ctors();
 }
