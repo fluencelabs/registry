7a530e4772812963463c1d93c1dbae5fc7ce48f7
Alexey Proshutinskiy <alexey.prosh@fluence.one>
2022-10-11 15:41:06 +0400

    update aqua

--- builtin-package/package.sh
+++ builtin-package/package.sh
@@ -9,6 +9,7 @@ SCHEDULED="${SCRIPT_DIR}/scheduled"
 (
     echo "*** compile scheduled scripts ***"
     cd ../aqua
+    npx aqua --version
     npx aqua --no-relay --air -i ./registry-scheduled-scripts.aqua -o "$SCHEDULED"
 )
 
