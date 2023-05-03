ff332be00cbb856b31e7198e0faf6c181c7658c6
Alexey Proshutinskiy <alexey.prosh@fluence.one>
2023-05-04 00:15:09 +0400

    feat(builtin-package): use new blueprint

--- builtin-package/package.sh
+++ builtin-package/package.sh
@@ -3,9 +3,13 @@ set -o pipefail -o nounset -o errexit
 
 # set current working directory to script directory to run script from everywhere
 cd "$(dirname "$0")"
-SCRIPT_DIR="$(pwd)"
-SCHEDULED="${SCRIPT_DIR}/scheduled"
+PACKAGE_DIR="$(pwd)/../registry"
+SCHEDULED="$PACKAGE_DIR/scheduled"
 
+(
+    rm -rf $PACKAGE_DIR
+    mkdir -p $PACKAGE_DIR
+)
 (
     echo "*** compile scheduled scripts ***"
     cd ../aqua
@@ -16,15 +20,24 @@ SCHEDULED="${SCRIPT_DIR}/scheduled"
 (
     echo "*** copy wasm files ***"
     cd ../service
-    cp artifacts/*.wasm "$SCRIPT_DIR"
+    cp artifacts/*.wasm "$PACKAGE_DIR"
 )
+
+REGISTRY_CID=$(ipfs add -q --only-hash --cid-version=1 --chunker=size-262144 $PACKAGE_DIR/registry.wasm)
+SQLITE_CID=$(ipfs add -q --only-hash --cid-version=1 --chunker=size-262144 $PACKAGE_DIR/sqlite3.wasm)
+mv $PACKAGE_DIR/registry.wasm "$PACKAGE_DIR"/"$REGISTRY_CID".wasm
+mv $PACKAGE_DIR/sqlite3.wasm "$PACKAGE_DIR"/"$SQLITE_CID".wasm
+cp registry_config.json "$PACKAGE_DIR"/"$REGISTRY_CID"_config.json
+cp sqlite3_config.json "$PACKAGE_DIR"/"$SQLITE_CID"_config.json
+
+# write blueprint.json
+echo "{}" | jq --arg registry_cid "$REGISTRY_CID" --arg sqlite_cid "$SQLITE_CID" '{"name": "registry", "dependencies":[{"/":$sqlite_cid},{"/":$registry_cid}]}' > "$PACKAGE_DIR/blueprint.json"
+
 
 (
     echo "*** create builtin distribution package ***"
     cd ..
-    mv builtin-package registry
-    tar --exclude="package.sh" -f registry.tar.gz -zcv ./registry
-    mv registry builtin-package
+    tar -f registry.tar.gz -zcv ./registry
 )
 
 echo "*** done ***"
