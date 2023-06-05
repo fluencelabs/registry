aada937e9911ec18cca56765397259ea314f1071
Maria Kuklina <maria@fluence.one>
2023-06-05 18:31:01 +0200

    Update crate content

--- distro/src/lib.rs
+++ distro/src/lib.rs
@@ -1,14 +1,17 @@
 pub const TRUST_GRAPH_WASM: &'static [u8] = include_bytes!("../registry-service/registry.wasm");
 pub const SQLITE_WASM: &'static [u8] = include_bytes!("../registry-service/sqlite3.wasm");
 pub const CONFIG: &'static [u8] = include_bytes!("../registry-service/Config.toml");
-pub const SCHEDULED_SCRIPT_1: &'static str = include_str!("../registry-service/air/registry-scheduled-scripts.clearExpired_86400.air");
-pub const SCHEDULED_SCRIPT_1_PERIOD: u32 = 86400;
+pub const CLEAR_EXPIRED_AIR: &'static str =
+    include_str!("../registry-service/air/registry-scheduled-scripts.clearExpired_86400.air");
+pub const CLEAR_EXPIRED_PERIOD_SEC: u32 = 86400;
 
-pub const SCHEDULED_SCRIPT_2: &'static str = include_str!("../registry-service/air/registry-scheduled-scripts.renew_43200.air");
-pub const SCHEDULED_SCRIPT_2_PERIOD: u32 = 43200;
+pub const RENEW_AIR: &'static str =
+    include_str!("../registry-service/air/registry-scheduled-scripts.renew_43200.air");
+pub const RENEW_PERIOD_SEC: u32 = 43200;
 
-pub const SCHEDULED_SCRIPT_3: &'static str = include_str!("../registry-service/air/registry-scheduled-scripts.replicate_3600.air");
-pub const SCHEDULED_SCRIPT_3_PERIOD: u32 = 3600;
+pub const REPLICATE_AIR: &'static str =
+    include_str!("../registry-service/air/registry-scheduled-scripts.replicate_3600.air");
+pub const REPLICATE_PERIOD_SEC: u32 = 3600;
 
 pub mod build_info {
     include!(concat!(env!("OUT_DIR"), "/built.rs"));
@@ -23,10 +26,28 @@ pub fn modules() -> std::collections::HashMap<&'static str, &'static [u8]> {
     }
 }
 
-pub fn scripts() -> Vec<(&'static str, u32)> {
+pub struct Script {
+    pub name: &'static str,
+    pub air: &'static str,
+    pub period_sec: u32,
+}
+
+pub fn scripts() -> Vec<Script> {
     vec![
-        (SCHEDULED_SCRIPT_1, SCHEDULED_SCRIPT_1_PERIOD),
-        (SCHEDULED_SCRIPT_2, SCHEDULED_SCRIPT_2_PERIOD),
-        (SCHEDULED_SCRIPT_3, SCHEDULED_SCRIPT_3_PERIOD)
+        Script {
+            name: "registry-clear-expired",
+            air: CLEAR_EXPIRED_AIR,
+            period_sec: CLEAR_EXPIRED_PERIOD_SEC,
+        },
+        Script {
+            name: "registry-renew",
+            air: RENEW_AIR,
+            period_sec: RENEW_PERIOD_SEC,
+        },
+        Script {
+            name: "registry-replicate",
+            air: REPLICATE_AIR,
+            period_sec: REPLICATE_PERIOD_SEC,
+        },
     ]
 }
