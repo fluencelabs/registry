92afff26f4fddfeeb69e1cd16f0e51d2c04a25d2
Maria Kuklina <maria@fluence.one>
2023-06-09 13:06:42 +0200

    try fix tests

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ mkdir -p "$DISTRO_TARGET"
 
 npx aqua --no-relay --air -i ./aqua/registry-scheduled-scripts.aqua -o "$DISTRO_TARGET/air"
 
-cp service/artifacts/registry.wasm service/artifacts/sqlite3.wasm service/Config.toml "$DISTRO_TARGET"
+cp service/artifacts/registry.wasm service/artifacts/sqlite3.wasm distro/Config.toml "$DISTRO_TARGET"
 
 cd distro
 cargo build
